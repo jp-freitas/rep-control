ddd8879a25898bb05b8af43d3bac0f6b6c55116a
TecJP <tec.joao.freitas@gmail.com>
2021-09-13 14:13:05 -0300

    Adding the global style of modals

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -32,7 +32,7 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body, input, textarea, button, a {
-    font-family: 'Roboto', sans-serif;
+    font-family: 'Glory', sans-serif;
     font-weight: 400;
     text-decoration: none;
     color: var(--text);
@@ -50,5 +50,43 @@ export const GlobalStyle = createGlobalStyle`
   [disabled] {
     opacity: 0.6;
     cursor: not-allowed;
+  }
+
+  .react-modal-overlay {
+    background: rgba(0, 0, 0, 0.6);
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .react-modal-content {
+    width: 100%;
+    max-width: 36rem;
+    background: var(--background);
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.5rem;
+  }
+
+  .react-modal-close {
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+    border: 0;
+    background: transparent;
+    transition: filter(0.2s);
+
+    &:hover {
+      filter: brightness(0.8);
+    }
+
+    > svg {
+      stroke-width: 4;
+    }
   }
 `;
