426950f7fffee2c3eb7c5a086697f7902a24b80d
TecJP <tec.joao.freitas@gmail.com>
2021-09-16 20:03:05 -0300

    Getting the id of rep from the firebase

--- src/pages/Rep/index.tsx
+++ src/pages/Rep/index.tsx
@@ -1,5 +1,17 @@
+import { Link, useParams } from "react-router-dom";
+
+type RepParams = {
+  id: string;
+}
+
 export function Rep() {
+  const params = useParams<RepParams>();
+  const repId = params.id;
+
   return (
-    <h1>Rep</h1>
+    <>
+      <h1>Rep{repId}</h1>
+      <Link to="/dashboard">Voltar</Link>
+    </>
   );
 }
