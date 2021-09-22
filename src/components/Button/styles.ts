e426c3d0933d2c860952b0df1a57920ad3f1cacc
TecJP <tec.joao.freitas@gmail.com>
2021-09-22 16:00:29 -0300

    Reduce the size of margin and padding

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -4,13 +4,13 @@ export const Container = styled.button`
     display: flex;
     align-items: center;
     justify-content: center;
-    height: 3rem;
+    height: 2.6rem;
     font-size: 1rem;
-    margin-top: 1rem;
-    padding: 1rem;
+    margin-top: 0.6rem;
+    padding: 0.6rem;
     background: var(--background-sidebar);
     border: 0;
-    border-radius: 0.5rem;
+    border-radius: 0.25rem;
     transition: filter 0.2s;
     box-shadow: 0 0.5rem 0.5rem -0.25rem rgba(0, 0, 0, 0.25);
 
