61ceb80d3114d51556eaf3510cad93247da4cb08
TecJP <tec.joao.freitas@gmail.com>
2021-09-22 16:03:13 -0300

    Adding the components Reps and pagination.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,53 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { database } from "../../services/firebase";
+
 import { Header } from "../../components/Header";
 import { NewRepModal } from "../../components/NewRepModal";
-import { TableContent } from "../TableContent";
+import { Reps } from "../../components/Reps";
+import { Pagination } from "../../components/Pagination";
+
 import { Container, Content } from "./styles";
+
+type FirebaseReps = Record<string, {
+  local: string;
+  internet_protocol: number;
+  serial_number: number;
+}>
+
+export type Rep = {
+  id: string;
+  local: string;
+  internet_protocol: number;
+  serial_number: number;
+}
 
 export function Dashboard() {
   const [isNewRepModalOpen, setIsNewRepModalOpen] = useState(false);
+
+  const [reps, setReps] = useState<Rep[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [repsPerPage] = useState(7);
+  const indexOfLastRep = currentPage * repsPerPage;
+  const indexOfFirstRep = indexOfLastRep - repsPerPage;
+  const currentReps = reps.slice(indexOfFirstRep, indexOfLastRep);
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+  useEffect(() => {
+    const repRef = database.ref(`reps/`);
+    repRef.on('value', rep => {
+      const databaseRep = rep.val();
+      const firebaseReps: FirebaseReps = databaseRep ?? {};
+      const parsedReps = Object.entries(firebaseReps).map(([key, value]) => {
+        return {
+          id: key,
+          local: value.local,
+          internet_protocol: value.internet_protocol,
+          serial_number: value.serial_number,
+        }
+      });
+      setReps(parsedReps);
+    });
+  }, []);
 
   function handleOpenNewRepModal() {
     setIsNewRepModalOpen(true);
@@ -20,7 +62,11 @@ export function Dashboard() {
       <Container>
         <Content>
           <Header onOpenNewRepModal={handleOpenNewRepModal} />
-          <TableContent />
+          <Reps reps={currentReps} />
+          <Pagination
+            repsPerPage={repsPerPage}
+            totalReps={reps.length}
+            paginate={paginate} />
         </Content>
         <NewRepModal
           isOpen={isNewRepModalOpen}
