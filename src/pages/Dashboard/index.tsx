eae71eb2f4a8f0598ad0831247e322a7fbaa37ab
TecJP <tec.joao.freitas@gmail.com>
2021-10-05 10:35:58 -0300

    functions to handle the new user modal

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,12 +2,22 @@ import { useState } from "react";
 
 import { Header } from "../../components/Header";
 import { NewRepModal } from "../../components/NewRepModal";
+import { NewUserModal } from "../../components/NewUserModal";
 import { Reps } from "../../components/Reps";
 
 import { Container, Content } from "./styles";
 
 export function Dashboard() {
   const [isNewRepModalOpen, setIsNewRepModalOpen] = useState(false);
+  const [isNewUserModalOpen, setIsNewUserModalOpen] = useState(false);
+
+  function handleOpenNewUserModal() {
+    setIsNewUserModalOpen(true);
+  }
+
+  function handleCloseNewUserModal() {
+    setIsNewUserModalOpen(false);
+  }
 
   function handleOpenNewRepModal() {
     setIsNewRepModalOpen(true);
@@ -21,9 +31,16 @@ export function Dashboard() {
     <>
       <Container>
         <Content>
-          <Header onOpenNewRepModal={handleOpenNewRepModal} />
+          <Header
+            onOpenNewRepModal={handleOpenNewRepModal}
+            onOpenNewUserModal={handleOpenNewUserModal}
+          />
           <Reps />
         </Content>
+        <NewUserModal
+          isOpen={isNewUserModalOpen}
+          onRequestClose={handleCloseNewUserModal}
+        />
         <NewRepModal
           isOpen={isNewRepModalOpen}
           onRequestClose={handleCloseNewRepModal}
